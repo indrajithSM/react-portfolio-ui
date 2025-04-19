f43fc0f0fabcd7a4a134bd010341a7c5e04b6d1e
indrajithSM <iamhacker1119@gmail.com>
2025-04-19 14:30:32 +0530

    vite change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // ✅ Add this line for Azure deployment
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
